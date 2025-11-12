d6f21cfc36b44ea13acddb584eb031c7f65e2b0c
tue-AqielOostenbrug <a.oostenbrug@student.tue.nl>
2025-11-12 16:55:41 +0100

    Finished user abstraction

---
+++ include/user.h
@@ -0,0 +1,25 @@
+#ifndef USER_H
+#define USER_H
+#define PASSWORD_MAX_LEN 30
+#define NICKNAME_MAX_LEN 30
+#define USERNAME_MAX_LEN 30
+#define REALNAME_MAX_LEN 30
+
+typedef struct _user_t {
+    char *password;
+    char *nickname;
+    char *username;
+    char *realname;
+} user_t;
+
+extern user_t *user_init(user_t *usr, char *psw, char *nck, char *usn, char *rln);
+extern int *user_destroy(user_t *usr);
+
+// Setters for user values
+extern int set_password(user_t *usr, char *psw);
+extern int set_nickname(user_t *usr, char *nck);
+extern int set_username(user_t *usr, char *usn);
+extern int set_realname(user_t *usr, char *rln);
+
+#endif
+
---
+++ src/linux/client/user.c
@@ -0,0 +1,67 @@
+#include <user.h>
+#include <string.h>
+
+int strcheck(char * str, int max) {
+    if (str == NULL) return 1;
+    int len = strlen(str);
+    return (len > max) ||
+    (len <= 0);
+}
+
+user_t * user_init(user_t * usr, char * psw, char * nck, char * usn, char * rln) {
+    if (
+        strcheck(psw, PASSWORD_MAX_LEN) ||
+        strcheck(nck, NICKNAME_MAX_LEN) ||
+        strcheck(usn, USERNAME_MAX_LEN) ||
+        strcheck(rln, REALNAME_MAX_LEN)
+    ) {
+        return NULL;
+    }
+    
+    usr->password = strdup(psw);
+    usr->nickname = strdup(nck);
+    usr->username = strdup(usn);
+    usr->realname = strdup(rln);
+
+    return usr;
+}
+
+int set_password(user_t * usr, char * psw) {
+    if ((usr == NULL) || strcheck(psw, PASSWORD_MAX_LEN)) {
+        return -1;
+    }
+    
+    usr->password = strdup(psw);
+
+    return 0;
+}
+
+int set_nickname(user_t * usr, char * nck) {
+    if ((usr == NULL) || strcheck(nck, PASSWORD_MAX_LEN)) {
+        return -1;
+    }
+    
+    usr->nickname = strdup(nck);
+
+    return 0;
+}
+
+int set_username(user_t * usr, char * usn) {
+    if ((usr == NULL) || strcheck(usn, PASSWORD_MAX_LEN)) {
+        return -1;
+    }
+    
+    usr->username = strdup(usn);
+
+    return 0;
+}
+
+int set_realname(user_t * usr, char * rln) {
+    if ((usr == NULL) || strcheck(rln, PASSWORD_MAX_LEN)) {
+        return -1;
+    }
+    
+    usr->username = strdup(rln);
+
+    return 0;
+}
